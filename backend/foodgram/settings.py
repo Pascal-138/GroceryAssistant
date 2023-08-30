a9816645f32c7fc7060384ff7eb6ab1007d7fe8f
pascal-163 <aleksid92@gmail.com>
2023-08-30 22:26:33 +0700

    Second part

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-123')
 
 DEBUG = str(os.getenv('DEBUG', False)).lower() == 'true'
 
@@ -56,12 +56,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
