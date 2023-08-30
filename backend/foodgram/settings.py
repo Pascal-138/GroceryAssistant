2ed0c137f37e696c3fa3775518038867d1d74444
pascal-163 <aleksid92@gmail.com>
2023-08-30 20:37:06 +0700

    Second part

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -126,6 +126,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
