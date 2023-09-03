e8635ff930e3ab7f77864fc95db175c703e76b6f
pascal-163 <aleksid92@gmail.com>
2023-09-03 13:53:30 +0700

    Second part

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,18 +1,14 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-123')
+SECRET_KEY = 'django-insecure-3(g17ci^vo(eey#m*02=5be%3k%)q_h)uhda93zo9@wvu42oz1'
 
-DEBUG = str(os.getenv('DEBUG', False)).lower() == 'true'
+DEBUG=True
 
-allowed_hosts_str = os.getenv('ALLOWED_HOSTS', 'SECRET_HOSTS')
-ALLOWED_HOSTS = allowed_hosts_str.split(',')
+ALLOWED_HOSTS = ['51.250.102.155', '127.0.0.1', 'localhost', 'foodgrampy.hopto.org']
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
